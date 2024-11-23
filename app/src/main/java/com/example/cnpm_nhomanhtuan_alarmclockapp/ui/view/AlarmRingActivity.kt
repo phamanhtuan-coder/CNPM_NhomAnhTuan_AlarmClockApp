e9c7af622b4de064e28ae198e552f3bd6336060f
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-11-23 22:38:20 +0700

    Fix bug RingScreen hiển thị khi báo thức tắt

--- app/src/main/java/com/example/cnpm_nhomanhtuan_alarmclockapp/ui/view/AlarmRingActivity.kt
+++ app/src/main/java/com/example/cnpm_nhomanhtuan_alarmclockapp/ui/view/AlarmRingActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.cnpm_nhomanhtuan_alarmclockapp.di.Graph
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AlarmRingActivity : AppCompatActivity() {
 
@@ -25,24 +27,24 @@ class AlarmRingActivity : AppCompatActivity() {
         }
 
         lifecycleScope.launch {
-            val isEnabled = Graph.repository.isAlarmEnabled(alarmId)
+            val isEnabled = withContext(Dispatchers.IO) {
+                Graph.repository.isAlarmEnabled(alarmId)
+            }
             Log.d("AlarmRingActivity", "Alarm ID: $alarmId, is_enabled: $isEnabled")
             if (!isEnabled) {
                 Log.d("AlarmRingActivity", "Alarm is not enabled, stopping activity.")
                 finish()
             } else {
                 Log.d("AlarmRingActivity", "Alarm is enabled, starting custom sound.")
+                setContent {
+                    AlarmRingScreen(
+                        alarmId = alarmId,
+                        getAlarmById = { id -> Graph.repository.getAlarmById(id) },
+                        onStopAlarm = { finish() }
+                    )
+                }
             }
         }
-
-        setContent {
-            AlarmRingScreen(
-                alarmId = alarmId,
-                getAlarmById = { id -> Graph.repository.getAlarmById(id) },
-                onStopAlarm = { finish() }
-            )
-        }
-
     }
 
     override fun onDestroy() {
